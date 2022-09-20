96292be9efe3ef44764af61da947a11499190804
Emiliano Rodriguez <emirod1955@gmail.com>
2022-09-20 04:26:04 -0700

    correcciones en resources y footer

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -24,7 +24,7 @@ function Resources (){
                 <div className="blockParagraph">
                     <h1>Lorem Ipsum</h1>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                    <input type="button" value="View our 3d model" />
+                    <input id="resourcesButton" type="button" value="View our 3d model" />
                 </div>
             </div>
             <div className="block3">
@@ -32,8 +32,8 @@ function Resources (){
                     <h1>Code</h1>
                     <p id="blockParagraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <div className="buttons">
-                        <a href="https://github.com/unnamedNasaSpaceApps/Moonquake-web" target="_blank" rel="noopener noreferrer"><input type="button" value="Web code" /></a>
-                        <a href="https://github.com/unnamedNasaSpaceApps/Moonquake-Map" target="_blank" rel="noopener noreferrer"><input type="button" value="Sim code" /></a>
+                        <a href="https://github.com/unnamedNasaSpaceApps/Moonquake-web" target="_blank" rel="noopener noreferrer"><input id="resourcesButton" type="button" value="Web code" /></a>
+                        <a href="https://github.com/unnamedNasaSpaceApps/Moonquake-Map" target="_blank" rel="noopener noreferrer"><input id="resourcesButton" type="button" value="Sim code" /></a>
                     </div>
                 </div>
                 <img id="flaticonImg" src={code} alt="" />
