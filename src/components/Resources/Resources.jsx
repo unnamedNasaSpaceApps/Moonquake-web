9a7c29fe94c6de8cdfbd3ab20b942044fcb98275
emirod <emirod@penguin>
2022-09-24 13:15:30 -0300

    resources finalizado

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -14,6 +14,7 @@ function Resources (){
         <div className="resources">
             <div className="block1">
                 <div className="blockParagraph">
+                    <h1>The Challenge</h1>
                     <p>When they explored the Moon, NASA’s Apollo astronauts left behind several instruments to collect geophysical data near each Apollo landing site. Your challenge is to develop an app that plots the seismic data these instruments transmitted back to Earth on an interactive 3-D globe.</p>
                 </div>
                 <img id="flaticonImg" src={sismo} alt="" />
@@ -21,16 +22,18 @@ function Resources (){
             <div className="block2">
                 <img src={moon} alt="" />
                 <div className="blockParagraph">
+                    <h1>Lorem Ipsum</h1>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                     <input type="button" value="View our 3d model" />
                 </div>
             </div>
             <div className="block3">
                 <div className="blockParagraph">
+                    <h1>Code</h1>
                     <p id="blockParagraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <div className="buttons">
-                        <input type="button" value="Web code" />
-                        <input type="button" value="Sim code" />
+                        <a href="https://github.com/unnamedNasaSpaceApps/Moonquake-web" target="_blank" rel="noopener noreferrer"><input type="button" value="Web code" /></a>
+                        <a href="https://github.com/unnamedNasaSpaceApps/Moonquake-Map" target="_blank" rel="noopener noreferrer"><input type="button" value="Sim code" /></a>
                     </div>
                 </div>
                 <img id="flaticonImg" src={code} alt="" />
