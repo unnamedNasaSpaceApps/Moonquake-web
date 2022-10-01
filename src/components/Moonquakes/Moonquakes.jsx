74fb41b64bb1b18faaaa8dc6fa723d91735179c9
Emiliano Rodriguez <emirod1955@gmail.com>
2022-10-01 05:19:09 -0700

    ultima

--- src/components/Moonquakes/Moonquakes.jsx
+++ src/components/Moonquakes/Moonquakes.jsx
@@ -5,44 +5,79 @@ import React from "react";
 import './Moonquakes.css';
 
 //import router libraly
-import {Link} from "react-router-dom";
+/*import {Link} from "react-router-dom";*/
 
 function Moonquakes (){
     return(
         <div className="Moonquakes">
-            <div className="moonquakeBlock1">
-                <h1>Apollo 11</h1>
-                <div className="moonquakeParagraph">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <div className="apollo11Block">
+                <div className="titleBox"><h1>Apollo 11</h1></div>
+                <div className="table">
+                    <ul>
+                        <li className="subTable">Station</li>
+                        <li className="subTable">Latitude</li>
+                        <li className="subTable">Longitude</li>
+                        <li className="subTable">Scale</li>
+                        <li className="subTable">ScaleFreq</li>
+                        <li className="subTable">StartTime</li>
+                        <li className="subTable">EndTime</li>
+                    </ul>
                 </div>
             </div>
-
-            <div className="moonquakeBlock2">
+            <div className="apollo12Block">
                 <h1>Apollo 12</h1>
-                <div className="moonquakeParagraph">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>  
+                <div className="table">
+                    <ul>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                    </ul>
                 </div>
             </div>
-
-            <div className="moonquakeBlock3">
+            <div className="apollo14Block">
                 <h1>Apollo 14</h1>
-                <div className="moonquakeParagraph">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <div className="table">
+                    <ul>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                    </ul>
                 </div>
             </div>
-
-            <div className="moonquakeBlock4">
+            <div className="apollo15Block">
                 <h1>Apollo 15</h1>
-                <div className="moonquakeParagraph">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <div className="table">
+                    <ul>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                    </ul>
                 </div>
             </div>
-
-            <div className="moonquakeBlock5">
-                <h1>The largest impact (Apollo 16)</h1>
-                <div className="moonquakeParagraph">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                    <Link to=""><input id="resourcesButton" type="button" value="more info" /></Link>
+            <div className="apollo16Block">
+                <h1>Apollo 16</h1>
+                <div className="table">
+                    <ul>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                        <li className="subTable"></li>
+                    </ul>
                 </div>
             </div>
         </div>
